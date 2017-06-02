5f7b46650d333928c62cd9ad606d08c841473e2a
nirajkvinit <nirajkvinit@yahoo.co.in>
2017-06-02 22:19:05 +0530

    Basic modifications

---
+++ dive-into-python3/ch3/ospathex.py
@@ -0,0 +1,3 @@
+#path = ~/code/python3-study/dive-into-python3/ch3/ospathex.py
+import os
+print(os.path.join(os.path.expanduser('~'), 'code', 'python3-study', 'dive-into-python3', 'ch3', 'ospathex.py'))
