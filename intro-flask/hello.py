c3da06610861bc4c27b31883a6b7804342dae767
Niraj Kumar <nirajkvinit@gmail.com>
2017-07-30 11:08:40 +0530

    ch3 completed

--- intro-flask/hello.py
+++ intro-flask/hello.py
@@ -1,5 +1,6 @@
 ''' hello.py test flask application '''
 from flask import Flask
+from flask import render_template
 # pylint: disable=invalid-name
 app = Flask(__name__)
 
@@ -7,12 +8,14 @@ app = Flask(__name__)
 @app.route('/')
 def index():
 	''' serving default route / '''
-	return "<h1>Hello, world!</h1>"
+	#return "<h1>Hello, world!</h1>"
+	return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
 	''' serving route /user/<name> This route will return Hello <name>'''
-	return '<h1>Hello, {0}!</h1>'.format(name)
+	#return '<h1>Hello, {0}!</h1>'.format(name)
+	return render_template('user.html', name=name)
 
 
 if __name__ == '__main__':
