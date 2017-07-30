3904a4d9dbe7f86a2779f6791367296e377e491b
Niraj Kumar <nirajkvinit@gmail.com>
2017-07-30 18:01:01 +0530

    treeoopy initial commit

---
+++ tree-oopy/monster.py
@@ -0,0 +1,10 @@
+class Monster:
+
+	def __init__(self, **kwargs):
+		self.hit_points = kwargs.get('hit_points', 1)
+		self.weapon = kwargs.get('weapon', 'sword')
+		self.color = kwargs.get('color', 'yellow')
+		self.sound = kwargs.get('sound', 'roar')
+
+	def battlecry(self):
+		return self.sound.upper()
