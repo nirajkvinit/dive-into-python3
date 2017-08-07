99e5a809b8056f6cd8eca2196257728283637eeb
Niraj Kumar <nirajkvinit@gmail.com>
2017-08-07 17:32:32 +0530

    Created first URL and view

--- intro-django/readit/books/views.py
+++ intro-django/readit/books/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def list_books(request):
+	return HttpResponse('Logged in user: ' + request.user.username)
