a8ec5927692ae04b96e06bb66d3d33e6c2124099
Niraj Kumar <nirajkvinit@gmail.com>
2017-08-08 15:09:48 +0530

    Add create author page, refactor form templates

--- intro-django/readit/readit/urls.py
+++ intro-django/readit/readit/urls.py
@@ -16,11 +16,13 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from books.views import AuthorList, list_books, BookDetail, AuthorDetail, ReviewList, review_book
+from books.views import (AuthorList, list_books, BookDetail, AuthorDetail, 
+    ReviewList, review_book, CreateAuthor)
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', list_books, name='books'),
+    url(r'^authors/add/$', CreateAuthor.as_view(), name='add-author'),
     url(r'^authors/$', AuthorList.as_view(), name='authors'),
     url(r'^books/(?P<pk>[-\w]+)/$', BookDetail.as_view(), name='book-detail'),
     url(r'^authors/(?P<pk>[-\w]+)/$', AuthorDetail.as_view(), name='author-detail'),
